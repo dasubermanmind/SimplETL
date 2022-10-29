33e83191841aada73a0601d82e76c0fcec5230e1
dasubermanmind <ero33@drexel.edu>
2022-10-29 16:03:27 -0400

    Temp WIP for extract

--- interface/SimplETL.py
+++ interface/SimplETL.py
@@ -2,6 +2,9 @@
 from typing import Any, List
 import logging
 
+from pandas import read_csv
+import pandas as pd
+
 class SimplETL:
     def __init__(self, project_name, environment, endpoint) -> None:
         self.project_name = project_name
@@ -21,7 +24,12 @@ class SimplETL:
     # TODO: Need to investigate on whether or not 
     # I should always return a df
     def extract(self)-> List[Any]:
-        pass
+        columns = ['covid', 'infection_rate']
+        # setup all the files in data folder
+        # df = pd.DataFrame()
+        # TODO: Research other operations I can do with read_csv
+        df = pd.read_csv('filepath', header=columns)
+        return df
     
     
     def transform(self, data: List[Any])-> List[Any]:
