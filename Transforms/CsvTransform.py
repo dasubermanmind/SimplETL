3d3628dd74ba0689aade49b3a3e6daf28ce3224b
dasubermanmind <ero33@drexel.edu>
2022-11-20 15:29:16 -0500

    WIP: Started refactoring out to a base class

--- Transforms/CsvTransform.py
+++ Transforms/CsvTransform.py
@@ -29,32 +29,7 @@ class CsvTransform:
         data = pd.read_csv(csv)
         return data
 
-    def transform(self, data) -> Tuple[pd.DataFrame, list]:
-        """
-        This layer is responsible for extracting out column headers, data cleaning & any misc tasks 
-        that are needed
-
-        :param data Dataframe
-
-        Returns Dataframe
-
-        """
-
-        if len(data) <= 0:
-            self.logger.info('Failed to extract properly')
-
-        df = pd.DataFrame(data)
-        column_headers = list(df.columns.values)
-
-        df.fillna('No data provided at this time', inplace=True)
-        df.columns = column_headers
-
-        print(df)
-        # TODO: Update normalize fn
-        # df = normalize_df(df)
-
-        return df, column_headers
-
+    
     def execute(self, data, data_source_name) -> None:
         """
             This is the execution loop for the extraction
@@ -83,16 +58,4 @@ class CsvTransform:
             except StopIteration:
                 break
 
-    def load(self, data, name) -> None:
-        """
-            :param data Dataframe
-
-            Returns None
-        """
-        db = create_engine(connect())
-        print(f'Sql Alchemy Engine up and running...{db}')
-        create_table_on_headers(data, db, name)
-        # insert next
-        insert(engine, data, name)
-        # After inserted we can now "Finish" the ETL and give back statistics on the ingest
-        return None
+    
--- Transforms/Optimus.py
+++ Transforms/Optimus.py
@@ -1,4 +1,10 @@
 from abc import abstractclassmethod
+from typing import Tuple
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+from db.postgres import connect, create_table_on_headers, insert
 
 
 """
@@ -11,11 +17,9 @@ from abc import abstractclassmethod
 """
 
 
-class Optimus:
-    
-    
-    
+class Optimus:    
     def __init__(self):
+        #TODO: Migrate all instance variables from csv
         pass
 
     # These are part of the contract. These must be implemented for each transformer
@@ -28,10 +32,46 @@ class Optimus:
     def extract(self):
         raise NotImplementedError
     
-    def Transform(self, data):
-        pass
+    def transform(self, data) -> Tuple[pd.DataFrame, list]:
+        """
+        This layer is responsible for extracting out column headers, data cleaning & any misc tasks 
+        that are needed
+
+        :param data Dataframe
+
+        Returns Dataframe
+
+        """
+
+        if len(data) <= 0:
+            self.logger.info('Failed to extract properly')
+
+        df = pd.DataFrame(data)
+        column_headers = list(df.columns.values)
+
+        df.fillna('No data provided at this time', inplace=True)
+        df.columns = column_headers
+
+        print(df)
+        # TODO: Update normalize fn
+        # df = normalize_df(df)
+
+        return df, column_headers
+
     
     
-    def load(self, data):
-        pass
+    def load(self, data, name) -> None:
+        """
+            :param data Dataframe
+
+            Returns None
+        """
+        db = create_engine(connect())
+        print(f'Sql Alchemy Engine up and running...{db}')
+        create_table_on_headers(data, db, name)
+        # insert next
+        insert(db, data, name)
+        # After inserted we can now "Finish" the ETL and give back statistics on the ingest
+        return None
+
 
