c92e9e704c54838a078b59611eb235da4b322118
dasubermanmind <ero33@drexel.edu>
2022-11-22 18:21:45 -0500

    started refactoring...transforms is started...I should pick up with transforms again next time too

--- Transforms/Optimus.py
+++ Transforms/Optimus.py
@@ -1,4 +1,5 @@
 from abc import abstractclassmethod
+import logging
 from typing import Tuple
 
 import pandas as pd
@@ -18,13 +19,15 @@ from db.postgres import connect, create_table_on_headers, insert
 
 
 class Optimus:    
-    def __init__(self):
-        #TODO: Migrate all instance variables from csv
-        pass
+    def __init__(self, project_name, environment, endpoint):
+        self.project_name = project_name
+        self.environment = environment
+        self.endpoint = endpoint
+        self.logger = logging.basicConfigZ(level=logging.DEBUG)
 
     # These are part of the contract. These must be implemented for each transformer
     @abstractclassmethod
-    def normalize(self , data):
+    def normalize(self , data: ...):
        raise NotImplementedError
     
     
@@ -32,7 +35,8 @@ class Optimus:
     def extract(self):
         raise NotImplementedError
     
-    def transform(self, data) -> Tuple[pd.DataFrame, list]:
+    # TODO: Figure out what the generic fn signature should be
+    def transform(self, data) -> ...:
         """
         This layer is responsible for extracting out column headers, data cleaning & any misc tasks 
         that are needed
@@ -42,21 +46,28 @@ class Optimus:
         Returns Dataframe
 
         """
-
-        if len(data) <= 0:
+        if len(data) <= 0 or data is None:
             self.logger.info('Failed to extract properly')
 
+        # Transform doc is the generic transform that will be used for all of the 
+        # transformers, the impl for the other transformers should have a normalize fn 
+        # that takes care of the batched datum.
+        transform_doc = {}
+        
         df = pd.DataFrame(data)
         column_headers = list(df.columns.values)
 
         df.fillna('No data provided at this time', inplace=True)
         df.columns = column_headers
 
-        print(df)
-        # TODO: Update normalize fn
-        # df = normalize_df(df)
+        df = self.normalize_df(df)
+        
+        transform_doc['_idx'] = df
+        
+        print(transform_doc)
 
-        return df, column_headers
+        # TODO: Investigate if I should cast transform_doc to a list or something (list[tuples])
+        return transform_doc, column_headers
 
     
     
